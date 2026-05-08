86f41bac704138854c60b1938d5a65dae33ff606
BlueStarVN101 <tinleducdev@gmail.com>
2026-05-08 13:33:27 +0700

    add excercises

---
+++ DSA/Javascript/1.js
@@ -0,0 +1,37 @@
+/* Chef Mario's Recipe Book 
+Chef Mario was in the middle of writing his cookbook masterpiece
+when he spilled coffee on his keyboard! Now all his recipes have repeat
+ingredients.
+
+Help save Chef Mario's cookbook by writing a function that takes in an array 
+and returns a new array with all the duplicates removed. 
+
+Example input: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops", "🦄 unicorn", "🍭 lollipops"];
+Example output: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops"];
+*/ 
+
+const eggScrambleRecipe = [
+            "🥓 bacon",
+            "🥓 bacon", 
+            "🍳 eggs",
+            "🫑 green peppers",
+            "🧀 cheese",
+            "🌶️ hot sauce",
+            "🥓 bacon",
+            "🥦 broccoli", 
+            "🧀 cheese",
+            "🥦 broccoli", 
+            "🌶️ hot sauce"
+        ]
+
+function removeDupesFromArray(arr){
+    let uniqueIngredients = [];
+    arr.forEach(element => {
+        if (!uniqueIngredients.includes(element)){
+            uniqueIngredients.push(element);
+        }
+    });
+    return uniqueIngredients;
+}
+
+console.log(removeDupesFromArray(eggScrambleRecipe));
---
+++ DSA/Javascript/4.js
@@ -0,0 +1,3 @@
+/**
+ * create function that takes in an array and returns a new array with all the duplicates removed
+ */
